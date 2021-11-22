8a82ed037501c383cecd6b7a0971e60fdd95275a
iiscsanjay <iet.sanjay@gmail.com>
2021-11-22 13:14:38 -0600

    testing1

--- model/database.php
+++ model/database.php
@@ -1,7 +1,6 @@
 <?php
     $dsn = 'mysql:host=webdb.cgae1wu7ahk9.us-east-2.rds.amazonaws.com;dbname=webdb;port=3306';
     $username = 'admin';
-    $password = 'fireISout3473';
 
     try {
         $db = new PDO($dsn, $username, $password);
